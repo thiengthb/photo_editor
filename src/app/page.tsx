0bc0397a6095fd9fd72382e6564f50b6a48cadb0
thi3n <136480142+thiengthb@users.noreply.github.com>
2024-09-28 19:58:47 +0700

    replace asset icons with fa icons, removing navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
 import Canvas from "@/components/Canvas";
-import Navbar from "@/components/Navbar";
-import { useState } from "react";
 
 export default function Home() {
-  const [shape, setShape] = useState("");
-
   return (
-    <div className="">
-      <Navbar setShape={setShape} />
-      <Canvas shape={shape} />
+    <div>
+      <div className="flex items-center justify-center">
+        <Canvas />
+      </div>
     </div>
   );
 }
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,55 +1,139 @@
-"use client";
-import React, { useEffect, useRef } from "react";
-import * as fabric from "fabric";
-import { drawCircle, drawRectangle, drawTriangle } from "@/drawingTool/shapes";
+"use client"
 
-export default function App({shape}) {
-  const canvasRef = useRef(null);
+import { useEffect, useRef, useState } from 'react';
+import * as fabric from 'fabric';
+import { Button } from '@nextui-org/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEraser, faSquare, faCircle, faLinesLeaning, faFont, faImage } from '@fortawesome/free-solid-svg-icons';
+
+export default function Canvas() {
+  const canvasRef = useRef(null); 
   const fabricCanvasRef = useRef(null);
+  const [isDrawingMode, setIsDrawingMode] = useState(false); 
+
+  const initCanvas = (canvasElement) => {
+    return new fabric.Canvas(canvasElement, {
+      height: window.innerHeight - 100,
+      width: window.innerWidth/2,
+      backgroundColor: 'white',
+    });
+  };
 
   useEffect(() => {
     const canvasElement = canvasRef.current;
 
     if (canvasElement) {
       fabricCanvasRef.current = initCanvas(canvasElement);
-      
-      switch(shape) {
-        case "circle":
-          drawCircle(fabricCanvasRef.current, "red");
-          break;
-        case "rectangle":
-          drawRectangle(fabricCanvasRef.current, "red");
-          break;
-        case "triangle":
-          drawTriangle(fabricCanvasRef.current, "red");
-          break;
-        default:
-          console.error("Invalid shape");
-      }
-
-      fabricCanvasRef.current.renderAll();
+      fabricCanvasRef.current.renderAll(); 
     }
 
     return () => {
       if (fabricCanvasRef.current) {
         fabricCanvasRef.current.dispose();
-        fabricCanvasRef.current = null;
+        fabricCanvasRef.current = null; 
       }
     };
-  }, [shape]);
+  }, []);
 
-  const initCanvas = (element) =>
-    new fabric.Canvas(element, {
-      height: 800,
-      width: 800,
-      backgroundColor: "pink",
-      selection: false,
-      renderOnAddRemove: true,
+  const addRectangle = () => {
+    const rect = new fabric.Rect({
+      left: 100,
+      top: 100,
+      fill: 'gray',
+      width: 60,
+      height: 60,
     });
+    fabricCanvasRef.current.add(rect);
+  };
+
+  const addCircle = () => {
+    const circle = new fabric.Circle({
+      radius: 30,
+      fill: 'gray',
+      left: 150,
+      top: 150,
+    });
+    fabricCanvasRef.current.add(circle);
+  };
+
+  const addLine = () => {
+    const line = new fabric.Line([50, 100, 200, 100], {
+      left: 170,
+      top: 150,
+      stroke: 'gray',
+      strokeWidth: 3,
+    });
+    fabricCanvasRef.current.add(line);
+  };
+
+  const addText = () => {
+    const text = new fabric.IText('Enter text', {
+      left: 100,
+      top: 200,
+      fill: 'black',
+      fontSize: 20,
+      editable: true,   
+      selectable: true, 
+    });
+    fabricCanvasRef.current.add(text);
+  };
+
+  const enableDrawing = () => {
+    if (fabricCanvasRef.current) {
+      const currentMode = !fabricCanvasRef.current.isDrawingMode; 
+      fabricCanvasRef.current.isDrawingMode = currentMode; 
+      setIsDrawingMode(currentMode); 
+    }
+  };
+
+  const deleteSelectedObject = () => {
+    const activeObject = fabricCanvasRef.current.getActiveObject();
+    if (activeObject) {
+      fabricCanvasRef.current.remove(activeObject);
+    }
+  };
+
+  useEffect(() => {
+    
+    const handleDelEnter = (e) => {
+      if (e.key === 'Delete') {
+        deleteSelectedObject();
+        e.preventDefault();
+      }
+    } 
+
+    window.addEventListener("keydown",  handleDelEnter);
+    return () => {
+      window.removeEventListener('keydown', handleDelEnter);
+    };
+  }, []);
 
   return (
     <div>
-      <canvas id="canvas" ref={canvasRef} />
+      <div className='p-2 gap-2 flex items-center'>
+        <Button radius='full' isIconOnly onClick={addRectangle}>
+          <FontAwesomeIcon icon={faSquare} />
+        </Button>
+        <Button radius='full' isIconOnly onClick={addCircle}>
+        <FontAwesomeIcon icon={faCircle} /> 
+        </Button>
+        <Button radius='full' isIconOnly onClick={addLine}>
+          <FontAwesomeIcon icon={faLinesLeaning} />
+        </Button>
+        <Button radius='full' isIconOnly onClick={addText}>
+          <FontAwesomeIcon icon={faFont} />
+        </Button>
+        <Button onClick={enableDrawing}>
+          {isDrawingMode ? 'Disable' : 'Enable'} Drawing
+        </Button>
+        <Button isIconOnly onClick={deleteSelectedObject}>
+          <FontAwesomeIcon icon={faEraser} />
+        </Button>
+        <Button radius='full' isIconOnly onClick={""}>
+          <FontAwesomeIcon icon={faImage} />
+        </Button>
+      </div>
+      <canvas className="border-1 border-[#acacac] shadow-lg" ref={canvasRef} id="canvas" />
     </div>
   );
 }
